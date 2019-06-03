91afe5040a45c8d97a4c0574df491d14665dea71
harrythuku <harsygeorgee@gmail.com>
2019-06-03 18:12:40 +0300

    final

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from app import db
 from app.models import User, BlogPost, Image, Comment
 from flask_migrate import Migrate, MigrateCommand
 
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 migrate = Migrate(app, db)
