0973198c468274405a8a1ce971f62f790668cfe0
harrythuku <harsygeorgee@gmail.com>
2019-06-03 17:56:02 +0300

    adds the app settings

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+class Config():
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lorddoyo:king@localhost/hblogs'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    DEBUG = True
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    pass
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}
